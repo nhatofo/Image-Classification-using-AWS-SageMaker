ba0b10988fc5797ec82e3412e5cfd8839345d839
Teofilo Chichume <nhatofo@gmail.com>
2022-03-21 23:57:19 +0200

    Another commit after review

--- train_model.py
+++ train_model.py
@@ -1,19 +1,26 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import argparse
+from PIL import ImageFile
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.models as models
-import torchvision.transforms as transforms
 import torch.nn.functional as F
-from torchvision import datasets, transforms, models
-from collections import OrderedDict
-import argparse
-import os
-import logging
-import sys
-from PIL import ImageFile
+import torchvision.transforms as transforms
 import smdebug.pytorch as smd
+from collections import OrderedDict
+import logging
+import os
+import json
+import time
+import sys
+from torchvision import datasets
+from smdebug import modes
+from smdebug.profiler.utils import str2bool
+from smdebug.pytorch import get_hook
 
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -52,46 +59,76 @@ def test(model, test_loader, criterion, hook):
             total_loss, total_acc)
         )
     
-
-def train(model, trainloader, testloader, criterion, optimizer, hook):
+def train(model, train_loader, validation_loader, criterion, optimizer, epoch, device, hook):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    model.train()
-    hook.set_mode(smd.modes.TRAIN)
-    
-    epochs = 10
-    running_loss = 0
+    hook = get_hook(create_if_not_exists=True)
+    epoch = 3
+    best_loss = 1e6
+    image_dataset = {'train': train_loader, 'valid': validation_loader}
+    loss_counter = 0
 
-    for e in range(epochs):
-    
-        # Model in training mode, dropout is on
-        model.train()
-        
-        for inputs, labels in trainloader:
-            optimizer.zero_grad()
-            output = model(inputs)
-            loss = criterion(output, labels)
-            loss.backward()
-            optimizer.step()
-            
-            # Training Loss
-            _, preds = torch.max(output, 1)
-            running_loss += loss.item() * inputs.size(0)
-            correct_pred += torch.sum(preds == labels.data)
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+            running_samples = 0
+        # TODO: Finish the rest of the training code
+        # The code should stop training when the validation accuracy
+        # stops increasing
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
 
-            epoch_loss = running_loss // len(trainloader)
-            epoch_acc = correct_pred // len(trainloader)
-        
-            logger.info("\nEpoch: {}/{}.. ".format(e+1, epochs))
-            logger.info("Training Loss: {:.4f}".format(epoch_loss))
-            logger.info("Training Accuracy: {:.4f}".format(epoch_acc))
-        
-        test(model, testloader, criterion, hook)    
-    
-    return model 
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_samples += len(inputs)
+                if running_samples % 2000 == 0:
+                    accuracy = running_corrects/running_samples
+                    print("Images [{}/{} ({:.0f}%)] Loss: {:.2f} Accuracy: {}/{} ({:.2f}%)".format(
+                        running_samples,
+                        len(image_dataset[phase].dataset),
+                        100.0 * (running_samples /
+                                 len(image_dataset[phase].dataset)),
+                        loss.item(),
+                        running_corrects,
+                        running_samples,
+                        100.0*accuracy,
+                    )
+                    )
+
+                # NOTE: Comment lines below to train and test on whole dataset
+                if running_samples > (0.2*len(image_dataset[phase].dataset)):
+                    break
+
+            epoch_loss = running_loss / running_samples
+            epoch_acc = running_corrects / running_samples
+
+            if phase == 'valid':
+                if epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                else:
+                    loss_counter += 1
+
+        if loss_counter == 1:
+            break
+    return model
     
 def net():
     '''
@@ -153,13 +190,15 @@ def main(args):
     '''
     TODO: Initialize a model by calling the net function
     '''
-    model=net()
-    
-    # Register the SMDebug hook to save output tensors.
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = net()
     hook = smd.Hook.create_from_json_file()
     hook.register_hook(model)
+    epoch=10
     
-    train_loader, test_loader, validation_loader=create_data_loaders(args.data, args.batchsize)
+    
+    train_data_loader, test_loader, validation_loader=create_data_loaders(args.data, args.batchsize)
+    
     
     '''
     TODO: Create your loss and optimizer
@@ -172,7 +211,8 @@ def main(args):
     Remember that you will need to set up a way to get training data from S3
     '''
     logger.info("Start Model Training")
-    model=train(model, train_loader, test_loader, loss_criterion, optimizer, hook)
+    model=train(model, train_data_loader, validation_loader,
+          loss_criterion, optimizer, epoch, device, hook)
     
     '''
     TODO: Test the model to see its accuracy
@@ -202,8 +242,17 @@ if __name__=='__main__':
     )
     
     parser.add_argument(
+        "--epochs",
+        type=int,
+        default=14,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    
+    parser.add_argument(
         "--lr", type=float, default=0.1, metavar="LR", help="learning rate (default: 1.0)"
     )
+    
     
     parser.add_argument('--data', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
     parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
